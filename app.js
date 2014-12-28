36e9a9fdd2a20c90c764f68587f006dd7f59ae87
Ilya Manyahin <i.manyahin@gmail.com>
2014-12-29 01:56:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('express')(),
     http = require('http');
 
+// Url of the medium size data
 var data_url = 'http://storage.googleapis.com/structure.rollout.io/development/53ff41229a15ba0000000004/540d70434d9cbc6b82000f43/i386/412c2c19f8336ea14aae8ad1a32944ce';
 
 var data_source, data;
